5e5e3033d7c0b7e19c4d7bc2c76eec0ccc462813
Marçal Junior <marssaljr@gmail.com>
2021-12-19 16:06:55 -0300

    chore: alter field livro on produto

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Produto(models.Model):
 	nome = models.CharField('Nome', max_length=100)
 	subtitulo = models.CharField('Subtitulo', max_length=200, blank=True)
-	livro = models.FileField(upload_to='mediafiles/')
+	livro = models.FileField(upload_to='')
 	sinopse = models.CharField('Sinopse', max_length=1660)
 	imagem = models.CharField('Url', max_length=600, blank=True)
 	link_real = models.CharField('Loja', max_length=600, blank=True)
