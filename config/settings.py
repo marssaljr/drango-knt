8ab406ac8b4149de79589a32495b9789a16cab46
Marçal Junior <62912704+marssaljr@users.noreply.github.com>
2022-01-13 13:03:52 -0300

    chore: update settings.py

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/' #desenvolvimento
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#produção
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')#produção
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+
 MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR, 'mediafiles')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 LOGOUT_REDIRECT_URL = 'index'
